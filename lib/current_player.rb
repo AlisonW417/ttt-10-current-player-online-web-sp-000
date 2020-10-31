9e44e7564bf374bae136a6139f9ef39a61236b60
Alison Williams <apw5011@gmail.com>
2020-10-31 16:00:35 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,7 +2,7 @@ def turn_count(board)
   board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   counter = 0
   board.each do |turn|
-    if #{turn} == "X" || "0"
+    if #{turn} != " "
     counter += 1
   end
 end
